2880577eb523e6935465e5b4e9da045068a912aa
Mohamad Nesta Fazarahman <87962387+MohamadNesta@users.noreply.github.com>
2021-08-28 16:16:28 +0800

    Add files via upload
    +Footer
    +Roguci

--- roguci/app.js
+++ roguci/app.js
@@ -1,23 +1,11 @@
 const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-
-  burger.addEventListener("click", () => {
-    //Toggle Nav
-    nav.classList.toggle("nav-active");
-
-    //Animate Links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = "navLinkFade 0.5s ease forwards ${index / 7}s";
-      }
-    });
-    //Burger Animation
-    burger.classList.toggle("toggle");
+  const burger = document.getElementById("burger");
+  const nav = document.getElementById("nav-links");
+  [...nav.children].forEach((link, index) => {
+    link.style.animationDelay = `${index / 4 + 0.2}s`;
   });
+  //togle nav
+  burger.addEventListener("click", _ => burger.classList.toggle("toggle"));
 };
 
 navSlide();
